6391346fa273cf08f9745074535a98db99473e9f
yanghyeonjin <guswls7081@gmail.com>
2024-07-26 14:20:43 +0900

    사용자 입력 프로그램 작성
    - 1번부터 4번까지는 메뉴
    - 9번은 프로그램 종료

--- src/main/java/com/ohgiraffers/run/Application.java
+++ src/main/java/com/ohgiraffers/run/Application.java
@@ -1,7 +1,38 @@
 package com.ohgiraffers.run;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("=== 비품 관리 프로그램이 시작되었습니다. ===");
+            System.out.println("1: 비품의 재고 확인");
+            System.out.println("2: 비품 구매");
+            System.out.println("3: 비품 사용");
+            System.out.println("4: 법카 잔여 한도 확인");
+            System.out.println("9: 프로그램 종료");
+            System.out.print("원하시는 메뉴를 입력하세요: ");
+
+            int menuNo = scanner.nextInt();
+
+            switch (menuNo) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 9:
+                    System.out.println("프로그램을 종료합니다.");
+                    return;
+                default:
+                    System.out.println("번호를 잘못 입력하셨습니다.");
+            }
+        }
     }
 }
